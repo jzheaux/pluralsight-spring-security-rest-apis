7fba71b3252c870f1bd3e33a6c96e74adba36cd8
Josh Cummings <josh.cummings@gmail.com>
2020-11-06 19:48:28 -0700

    Single Page App

---
+++ app/src/main/resources/static/pc.js
@@ -0,0 +1,30 @@
+$( document ).ajaxSuccess((event, xhr, objects, data) => {
+    pc.up = () => pc._up(pc.root + "/up");
+    $("#up-arrow").addClass("enabled")[0].onclick = pc.up;
+
+    pc.down = () => pc._down(pc.root + "/down");
+    $("#down-arrow").addClass("enabled")[0].onclick = pc.down;
+});
+
+const pc = {
+    root: "http://127.0.0.1:8180/pc",
+    read: () => $.ajax(pc.root,
+            {
+                method: 'GET',
+                success: (data) => $("#pc").html(data.pc)
+            }),
+    _up: (url) => $.ajax(url,
+            {
+                method: 'POST',
+                success: (data) => $("#pc").html(data.pc)
+            }),
+    _down: (url) => $.ajax(url,
+            {
+                method: 'POST',
+                success: (data) => $("#pc").html(data.pc)
+            })
+};
+
+$(() => {
+    pc.read();
+});
